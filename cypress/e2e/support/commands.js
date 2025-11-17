a16b180478eb8a3a403e4ac6fa94a485043db319
Luuiza25 <89809445+Luuiza25@users.noreply.github.com>
2025-11-17 09:21:55 -0300

    Update commands.js

--- cypress/e2e/support/commands.js
+++ cypress/e2e/support/commands.js
@@ -1,1 +1,12 @@
+Cypress.Commands.add('createUser', (user) => {
+  return cy.request({ method: 'POST', url: '/usuarios', body: user, failOnStatusCode: false })
+})
+
+Cypress.Commands.add('login', (email, password) => {
+  return cy.request({ method: 'POST', url: '/login', body: { email, password }, failOnStatusCode: false })
+})
+
+Cypress.Commands.add('createProduct', (product, token) => {
+  return cy.request({ method: 'POST', url: '/produtos', headers: { Authorization: token }, body: product, failOnStatusCode: false })
+})
 
