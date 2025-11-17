986c7f1271a56c405b7fca40361166dc9c40872d
Luuiza25 <89809445+Luuiza25@users.noreply.github.com>
2025-11-17 09:20:53 -0300

    Update cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -1,1 +1,10 @@
+const { defineConfig } = require('cypress')
+
+module.exports = defineConfig({
+  e2e: {
+    baseUrl: 'https://serverest.dev',
+    specPattern: 'cypress/e2e/**/*.spec.js',
+    supportFile: 'cypress/support/e2e.js'
+  }
+})
 
